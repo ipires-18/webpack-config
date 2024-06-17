292c0a62050717cdf4cbe8cb86f278b32be4398a
Ian Oliveira <ian-pires@hotmail.com>
2024-06-17 12:45:22 -0300

    feat: add plugins with webpack

---
+++ webpack-config-plugins/src/index.js
@@ -0,0 +1,7 @@
+import Heading from './components/heading';
+
+const heading = new Heading();
+
+heading.create('Página princial');
+
+console.log(process.env.API_KEY);
---
+++ webpack-config-plugins/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const DotenvPlugin = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify('1.0.0'),
+      PORT: JSON.stringify('8080'),
+    }),
+    new DotenvPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
+};
