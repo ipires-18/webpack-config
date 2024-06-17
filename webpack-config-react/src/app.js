8995e84e870b203f71389cb5b55b4ea9935490d9
Ian Oliveira <ian-pires@hotmail.com>
2024-06-17 15:20:00 -0300

    feat": add the integration react more webpack

---
+++ webpack-config-react/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Meu projeto em react e webpack</h1>
+        <p>Integrado com sucesso!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack-config-react/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    devMiddleware: {
+      publicPath: '/dist/',
+    },
+    port: 3000,
+    hot: 'only',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+};
