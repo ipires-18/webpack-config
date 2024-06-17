43d0fc2a557a9073ce56830e05c9361446c69fec
Ian Oliveira <ian-pires@hotmail.com>
2024-06-17 16:46:51 -0300

    feat: add module federation with webpack

---
+++ webpack-config-module-federation/packages/app/src/bootstrap.js
@@ -0,0 +1,6 @@
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ webpack-config-module-federation/packages/app/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'http://localhost:9001/',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    devMiddleware: {
+      publicPath: 'http://localhost:9001/',
+    },
+    port: 9001,
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [require.resolve('@babel/preset-react')],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+      title: 'App',
+    }),
+    new ModuleFederationPlugin({
+      name: 'App',
+      remotes: {
+        HomeApp: 'HomeApp@http://localhost:9002/remoteEntry.js',
+        ContactApp: 'ContactApp@http://localhost:9003/remoteEntry.js',
+      },
+    }),
+  ],
+};
